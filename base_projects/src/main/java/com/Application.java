02ee8e7ff93e57958870509eb382acbedd56479a
司徒彬 <situbin@foxmail.com>
2022-01-10 17:00:35 +0800

    2022年01月10日17:00:06 添加 spring-boot 基础依赖类，添加了一些常用依赖

---
+++ base_projects/src/main/java/com/Application.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com;
+/**
+ * @author 司徒彬
+ * @date 2022/1/10 16:08
+ */
+public class Application {
+}
